0bc1d08287c20f9f156ddfed03c7134bd32712f2
Joyce <jkamgang.ir2026@esaip.org>
2025-03-19 11:25:01 +0100

    avec blob service

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import flask, jsonify
+from flask import Flask, jsonify
+from azure.storage.blob import BlobServiceClient
+import os
 import json
 import yaml
 
-app = Flask(_name_)
+app = Flask(__name__)
 def load_data(filename):
     with open(filename, 'r', encoding = 'utf-8') as file:
         if filename.endswith('.json'):
@@ -15,12 +17,12 @@ def load_data(filename):
 
 @app.route('/api/events', methods=['GET'])
 def get_events():
-    data = load_data('app/static_data/events.json')
+    data = load_data('C:/Users/jkamgang.ir2026/Documents/cloud computing/Projet-cloud/app/static_data/events.json')
     return jsonify(data)
 
-@app.route('api/news', methods=['GET'])
+@app.route('/api/news', methods=['GET'])
 def get_news():
-    data = load_data('app/static/news.yaml')
+    data = load_data('C:/Users/jkamgang.ir2026/Documents/cloud computing/Projet-cloud/app/static_data/news.yaml')
     return jsonify(data)
 
 if __name__ == '__main__':
